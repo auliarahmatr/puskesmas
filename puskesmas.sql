4c47578ec4a2d49cdc534c9af1997cadc6808063
auliarahmatr <elvous12@gmail.com>
2020-05-20 23:59:58 +0700

    puskesmas

--- puskesmas.sql
+++ puskesmas.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.3
+-- version 4.7.4
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 29, 2020 at 05:03 PM
--- Server version: 10.1.37-MariaDB
--- PHP Version: 7.2.12
+-- Generation Time: 20 Mei 2020 pada 18.56
+-- Versi Server: 10.1.28-MariaDB
+-- PHP Version: 7.1.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -25,7 +25,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tb_dokter`
+-- Struktur dari tabel `tb_dokter`
 --
 
 CREATE TABLE `tb_dokter` (
@@ -33,20 +33,22 @@ CREATE TABLE `tb_dokter` (
   `nama_dokter` varchar(90) NOT NULL,
   `spesialis` varchar(50) NOT NULL,
   `alamat` text NOT NULL,
-  `no_telp` varchar(15) NOT NULL
+  `no_telp` varchar(15) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `password` varchar(128) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `tb_dokter`
+-- Dumping data untuk tabel `tb_dokter`
 --
 
-INSERT INTO `tb_dokter` (`id_dokter`, `nama_dokter`, `spesialis`, `alamat`, `no_telp`) VALUES
-('74e808a1-40bf-444c-b69b-1e6dd6705d1e', 'Dr. Aulia Rahmat R', 'Umum', 'JL. BATU', '08122244005');
+INSERT INTO `tb_dokter` (`id_dokter`, `nama_dokter`, `spesialis`, `alamat`, `no_telp`, `username`, `password`) VALUES
+('74e808a1-40bf-444c-b69b-1e6dd6705d1e', 'Dr. Aulia Rahmat R', 'Umum', 'JL. BATU', '08122244005', '', '');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tb_obat`
+-- Struktur dari tabel `tb_obat`
 --
 
 CREATE TABLE `tb_obat` (
@@ -56,19 +58,20 @@ CREATE TABLE `tb_obat` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `tb_obat`
+-- Dumping data untuk tabel `tb_obat`
 --
 
 INSERT INTO `tb_obat` (`id_obat`, `nama_obat`, `ket_obat`) VALUES
 ('224e9ef5-69cf-11ea-acae-00ff6990c3c7', 'EsterC', 'Vitamin C'),
 ('86071bfd-7511-11ea-b32f-00ff6990c3c7', 'Imboost', 'Suplemen Imun booster'),
 ('860797df-7511-11ea-b32f-00ff6990c3c7', 'Fufang', 'Suplemen'),
-('98bc1f09-3954-4b29-af61-310bed90bdd9', 'Glico', 'a');
+('98bc1f09-3954-4b29-af61-310bed90bdd9', 'Glico', 'a'),
+('f2454f14-f42d-4c3f-b3e5-be400335ae89', 'AA', 'a');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tb_pasien`
+-- Struktur dari tabel `tb_pasien`
 --
 
 CREATE TABLE `tb_pasien` (
@@ -84,28 +87,22 @@ CREATE TABLE `tb_pasien` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `tb_pasien`
+-- Dumping data untuk tabel `tb_pasien`
 --
 
 INSERT INTO `tb_pasien` (`id_pasien`, `nomor_identitas`, `nama_pasien`, `jenis_kelamin`, `tanggal_lahir`, `agama`, `alamat`, `nama_kk`, `no_telp`) VALUES
 ('13111292-fa63-4c0b-b935-2022f4ccad73', '112345', 'Angela', 'L', '0000-00-00', 'Islam', 'Jl Taman Purbakala Cipari', 'Asd', '21345'),
 ('141ddd1b-73ae-48a4-87cc-5838db430fe6', '111111111111111111111111', 'Andriana', 'P', '1999-12-15', 'Islam', 'JL. BATU', 'Andreno', '02111345'),
-('1468b8e1-4f56-47a2-a70d-aba1bcad730c', '444448968', 'Gold', 'P', '0000-00-00', '', 'QQE', '', '4441'),
 ('26363aa0-8974-47d4-8142-3a7e0ac9f0a2', '444448965', 'Copper', 'L', '0000-00-00', '', 'QQW', '', '443'),
-('35505e74-bc0a-489b-9564-a726273c01bf', '1111111432', 'Exynos', 'P', '1991-06-13', 'Islam', 'ESP', 'Exy', '111112'),
-('4d731eb9-cd93-4296-a294-8aeb3a0b4136', '444448969', 'Iron', 'L', '0000-00-00', '', 'QQE', '', '444'),
-('564b3193-7939-4879-9a63-3774ce446e2a', '231298888881', 'Loki', 'L', '0000-00-00', '', 'Surabaya', '', '2178888'),
 ('57939ed4-4af4-4787-9a03-b7d8430a07d3', '12223332765899', 'Adreno', 'L', '0000-00-00', '', 'XTZ', '', '12322'),
 ('aef15c63-ef7e-47f6-a83e-d483d1812e92', '23331212', 'Simonn', 'P', '0000-00-00', '', 'Jakarta', '', '22222'),
-('bea79f5c-6bb8-40e9-9d9a-c2cfa77a25a3', '111222233389796', 'Snapdragon', 'P', '0000-00-00', '', 'XDC', '', '334421'),
 ('c431617a-300c-497e-9637-64db4c892dc5', '556744823', 'Prism', 'L', '0000-00-00', '', 'FGF', '', '554'),
-('e94d99bc-4be5-451e-bc5f-658695cca0fa', '444448961', 'Iridium', 'P', '0000-00-00', '', 'AAA', '', '0001'),
-('fbb552bd-d8f8-498b-af43-31353d188a7f', '231298888873', 'Thor', 'L', '0000-00-00', '', 'Denpasar', '', '21788889');
+('e94d99bc-4be5-451e-bc5f-658695cca0fa', '444448961', 'Iridium', 'P', '0000-00-00', '', 'AAA', '', '0001');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tb_poliklinik`
+-- Struktur dari tabel `tb_poliklinik`
 --
 
 CREATE TABLE `tb_poliklinik` (
@@ -115,7 +112,7 @@ CREATE TABLE `tb_poliklinik` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `tb_poliklinik`
+-- Dumping data untuk tabel `tb_poliklinik`
 --
 
 INSERT INTO `tb_poliklinik` (`id_poli`, `nama_poli`, `gedung`) VALUES
@@ -125,7 +122,7 @@ INSERT INTO `tb_poliklinik` (`id_poli`, `nama_poli`, `gedung`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tb_rekammedis`
+-- Struktur dari tabel `tb_rekammedis`
 --
 
 CREATE TABLE `tb_rekammedis` (
@@ -139,7 +136,7 @@ CREATE TABLE `tb_rekammedis` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `tb_rekammedis`
+-- Dumping data untuk tabel `tb_rekammedis`
 --
 
 INSERT INTO `tb_rekammedis` (`id_rm`, `id_pasien`, `keluhan`, `id_dokter`, `diagnosa`, `id_poli`, `tgl_periksa`) VALUES
@@ -148,7 +145,7 @@ INSERT INTO `tb_rekammedis` (`id_rm`, `id_pasien`, `keluhan`, `id_dokter`, `diag
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tb_rm_obat`
+-- Struktur dari tabel `tb_rm_obat`
 --
 
 CREATE TABLE `tb_rm_obat` (
@@ -158,7 +155,7 @@ CREATE TABLE `tb_rm_obat` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `tb_rm_obat`
+-- Dumping data untuk tabel `tb_rm_obat`
 --
 
 INSERT INTO `tb_rm_obat` (`id`, `id_rm`, `id_obat`) VALUES
@@ -170,7 +167,7 @@ INSERT INTO `tb_rm_obat` (`id`, `id_rm`, `id_obat`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tb_user`
+-- Struktur dari tabel `tb_user`
 --
 
 CREATE TABLE `tb_user` (
@@ -182,7 +179,7 @@ CREATE TABLE `tb_user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `tb_user`
+-- Dumping data untuk tabel `tb_user`
 --
 
 INSERT INTO `tb_user` (`id_user`, `nama_user`, `username`, `password`, `level`) VALUES
@@ -250,11 +247,11 @@ ALTER TABLE `tb_rm_obat`
   MODIFY `id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
--- Constraints for dumped tables
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
 --
 
 --
--- Constraints for table `tb_rekammedis`
+-- Ketidakleluasaan untuk tabel `tb_rekammedis`
 --
 ALTER TABLE `tb_rekammedis`
   ADD CONSTRAINT `tb_rekammedis_ibfk_1` FOREIGN KEY (`id_dokter`) REFERENCES `tb_dokter` (`id_dokter`),
@@ -262,7 +259,7 @@ ALTER TABLE `tb_rekammedis`
   ADD CONSTRAINT `tb_rekammedis_ibfk_3` FOREIGN KEY (`id_poli`) REFERENCES `tb_poliklinik` (`id_poli`);
 
 --
--- Constraints for table `tb_rm_obat`
+-- Ketidakleluasaan untuk tabel `tb_rm_obat`
 --
 ALTER TABLE `tb_rm_obat`
   ADD CONSTRAINT `tb_rm_obat_ibfk_1` FOREIGN KEY (`id_rm`) REFERENCES `tb_rekammedis` (`id_rm`) ON DELETE CASCADE ON UPDATE CASCADE,
